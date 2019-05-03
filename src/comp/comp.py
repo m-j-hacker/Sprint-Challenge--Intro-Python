95d21a2ec3fa9e0d05522d63cf0dca58875b0561
m-j-hacker <mjhacker@gmail.com>
2019-05-03 15:01:01 -0500

    Fix: comp.py for test

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -38,12 +38,12 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [human.name for human in humans if human.name[0] in string.ascii_uppercase[2:6]]
+c = [human.name for human in humans if human.name[0] in string.ascii_uppercase[2:7]]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
 print("Ages plus 10:")
-d = [human.age for human in humans if human.age >= 10]
+d = [human.age + 10 for human in humans]
 print(d)
 
 # Write a list comprehension that creates a list of strings which are the name
